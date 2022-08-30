ad1b30716233635ecc4f9f17c6890ccde77ac363
Pseudo <v.pseudo.11@gmail.com>
2022-08-29 21:22:30 -0500

    Header 100%

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,17 +42,26 @@ const ImgLogo = styled.img`
     max-height: 100%;
     object-fit: cover;
 `
-const ListSkills = styled.ul`
+const ListSkills = styled.nav`
     margin: 0 30px;
-    padding: 0;
+    display: flex;
 `
 
 const ItemContainer = styled.a`
     text-decoration: none;
     font-size: 1.2rem;
-    padding: 0 15px;
+    padding: 10px 15px;
     font-style: bold;
     font-weight: 600;
-    color: var(--color-5);
+    color: var(--color-1);
+    line-height: normal;
+    transition: all .3s ease;
+    border-bottom: 2px solid transparent;
+    &:hover {
+        color: var(--color-5);
+        border-bottom: 3px solid var(--color-5);
+        border-radius: 5px;
+        padding: 5px 15px;
+    }
 `
 export default Header
