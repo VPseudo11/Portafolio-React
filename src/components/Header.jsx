c4c0f1da3cac0989e762527f0da6a08c0c379c2f
Pseudo <v.pseudo.11@gmail.com>
2022-08-29 20:40:40 -0500

    Start Briefcase

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './../assets/img/PseudoDeveloper.png'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
